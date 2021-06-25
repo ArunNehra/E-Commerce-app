8fa9a947e213ad524ac57e8712d379017f85e170
Arun.n <arun.n@taboola.com>
2021-06-25 17:54:10 +0530

    Cartitem, cart, removebutton, updatebuttons, links to home and cart are also added payment is remaining

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
-
+import {Link} from 'react-router-dom';
+import CartItem from './Cartitem/Cartitem';
 import useStyles from "./CartStyles";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
   const classes = useStyles();
   const EmptyCart = () => (
     <Typography variant="subtitle1">
-      You have no items in your shopping cart, start adding some!
+      You have no items in your shopping cart, 
+      <Link to='/' className={classes.link}>start adding some</Link>!
     </Typography>
   );
 
@@ -16,7 +18,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
           </Grid>
         ))}
       </Grid>
@@ -31,6 +33,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
@@ -53,7 +56,7 @@ const Cart = ({ cart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3">
+      <Typography className={classes.title} variant="h3" gutterBottom>
         Your Shopping Cart
       </Typography>
       {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
